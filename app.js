5792016cb87596ee77cb803fbf0a76d1f108e56a
Alex Bazlinton <abazlinton@gmail.com>
2017-08-10 10:56:05 +0100

    js dufus

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 window.onload = function() {
-  var body = document.getElementsByTagName("body")[0];
-  body.innerHTML("Loaded JavaScript");
+  var body = document.getElementsByTagName('body')[0];
+  body.innerHTML = "Loaded JavaScript";
 }
