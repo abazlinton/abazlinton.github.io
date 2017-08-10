cc46f34fb7a2d03251e0adb6170dddb69f7ae1d0
Alex Bazlinton <abazlinton@gmail.com>
2017-08-10 11:43:15 +0100

    test countdown fixed url

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+var timeLeft = 3;
+
+
 window.onload = function() {
-  var body = document.getElementsByTagName('body')[0];
-  body.innerHTML = "Loaded JavaScript";
+  countdown();
+  setInterval(countdown, 1000);
 }
+
+function countdown () {
+  var countdown = document.getElementById('countdown');
+  countdown.innerHTML = "Redirecting in " + timeLeft;
+  if (!timeLeft) {
+    window.location.replace("https://glacial-scrubland-75610.herokuapp.com/");
+  }
+  timeLeft--;
+} 
+
+
