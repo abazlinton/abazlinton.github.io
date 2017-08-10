e87abf0f9bea802522c73670612d5b0bc95bcc24
Alex Bazlinton <abazlinton@gmail.com>
2017-08-10 17:20:02 +0100

    timeout code in place

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-var timeLeft = 3;
-
+var herokuUrl = "http://wobble-mind.herokuapp.com";
+var interval;
+var noRequests = 0;
 
 window.onload = function() {
-  countdown();
-  setInterval(countdown, 1000);
+  interval = setInterval(request, 1000);
 }
 
-function countdown () {
-  var countdown = document.getElementById('countdown');
-  countdown.innerHTML = "Redirecting in " + timeLeft;
-  if (!timeLeft) {
-    window.location.replace("https://glacial-scrubland-75610.herokuapp.com/");
-  }
-  timeLeft--;
+function redirect () {
+  window.location.replace(herokuUrl);
+}
+
+function request () {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', herokuUrl);
+  
+  xhr.timeout = 2000; // time in milliseconds
+  
+  xhr.onload = function () {
+    clearInterval(interval);
+    redirect();
+  };
+  
+  xhr.ontimeout = function (e) {
+    var countdown = document.getElementById('countdown');
+    countdown.innerText = "Retries:" + noRequests;
+  };
+  xhr.send(null);
+  noRequests++;
 } 
 
 
