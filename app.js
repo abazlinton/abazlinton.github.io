1b44dc913787e83bbbbd0ac4041ae0a2a32d3136
Alex Bazlinton <abazlinton@gmail.com>
2017-08-11 12:00:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var herokuUrl = "https://wobble-mind.herokuapp.com";
+var herokuUrl = "https://glacial-scrubland-75610.herokuapp.com";
 var interval;
 var noRequests = 0;
 
 window.onload = function() {
-  interval = setInterval(request, 1000);
+  interval = setInterval(request, 2000);
 }
 
 function redirect () {
@@ -12,15 +12,20 @@ function redirect () {
 
 function request () {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', herokuUrl);
+  xhr.open('GET', "https://yqd5anxtug.execute-api.eu-west-1.amazonaws.com/prod/testLamdaApiGateway");
   
-  xhr.timeout = 2000; // time in milliseconds
+  xhr.timeout = 500; // time in milliseconds
   
   xhr.onload = function () {
+    console.log(state);
     clearInterval(interval);
-//     redirect();
+    redirect();
   };
   
+  xhr.onerror = function(e){
+    console.log(e);
+  }
+
   xhr.ontimeout = function (e) {
     var countdown = document.getElementById('countdown');
     countdown.innerText = "Retries:" + noRequests;
